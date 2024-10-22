362b3245d3d1a2d1f9141e1d834309c4e1212f5c
maxw12 <74052003+maxw12@users.noreply.github.com>
2024-10-22 16:44:08 +0100

    Calculator half working with styling

--- project123/src/pages/calculator.tsx
+++ project123/src/pages/calculator.tsx
@@ -3,8 +3,7 @@ import { ReactNode, useEffect, useState } from "react";
 
 export default function Calculator() {
     const [currentValue, setCurrentValue] = useState<string>("0");
-    const [previousResult, setPreviousResult] = useState<string>("0");
-    const [newResult, setNewResult] = useState<number>(0);
+    const [total, setTotal] = useState<number>(0);
     const [displayValue, setDisplayValue] = useState<string>("0");
     const [previousOperator, setPreviousOperator] = useState<string>("+");
 
@@ -13,140 +12,174 @@ export default function Calculator() {
         useState<boolean>(true);
 
     const handleNumber = (number: string) => {
-        setDisplayValue(number);
-        if (lastButtonPressedIsOperator == false) {
-            setDisplayValue(displayValue + number);
+        let newDisplayValue = "";
+        if (lastButtonPressedIsOperator == true) {
+            newDisplayValue = number;
+        } else if (lastButtonPressedIsOperator == false) {
+            newDisplayValue = displayValue + number;
         }
+        setDisplayValue(newDisplayValue);
         setLastButtonPressedIsOperator(false);
-        setCurrentValue(displayValue);
+        setCurrentValue(newDisplayValue);
     };
 
     useEffect(() => {
-        console.log("newResult:", newResult);
-    }, [newResult]);
+        console.log("total:", total);
+        console.log("prevOp", previousOperator);
+        console.log("current value", currentValue);
+    }, [total, previousOperator, currentValue]);
 
     // track calculation by storing previous operator using the current
-    const handleOperator = (operator: string) => {
+    const handleOperator = (operator: string): void => {
         let result: number = 0;
+        if (operator === "c") {
+            setDisplayValue("0");
+            setCurrentValue("0");
+            setTotal(0);
+            setPreviousOperator("+");
+            return;
+        }
+
         switch (previousOperator) {
             case "+":
-                result = newResult + parseInt(currentValue);
+                result = total + parseInt(currentValue);
                 break;
             case "-":
-                result = newResult - parseInt(currentValue);
+                result = total - parseInt(currentValue);
                 break;
             case "*":
-                result = newResult * parseInt(currentValue);
+                result = total * parseInt(currentValue);
                 break;
             case "/":
-                result = newResult / parseInt(currentValue);
+                result = total / parseInt(currentValue);
+                break;
+            case "=":
+                result = total;
                 break;
         }
 
-        setDisplayValue(String(result));
-        setNewResult(result);
-
+        setTotal(result);
         setLastButtonPressedIsOperator(true);
+        setDisplayValue(String(result));
         setPreviousOperator(operator);
+
+        if (operator === "=") {
+            setCurrentValue(String(0));
+        }
     };
 
     return (
         <>
-            <div className="flex max-w-xs mx-auto my-auto">Calculator page</div>
-            <div className="flex ml-auto mr-auto max-w-lg">
-                <div className="flex w-52 ">{displayValue}</div>
-                <ul>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("1")}
-                    >
-                        1
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("2")}
-                    >
-                        2
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("3")}
-                    >
-                        3
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("4")}
-                    >
-                        4
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("5")}
-                    >
-                        5
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("6")}
-                    >
-                        6
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("7")}
-                    >
-                        7
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("8")}
-                    >
-                        8
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("9")}
-                    >
-                        9
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleNumber("0")}
-                    >
-                        0
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleOperator("+")}
-                    >
-                        +
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleOperator("-")}
-                    >
-                        -
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleOperator("*")}
-                    >
-                        *
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleOperator("/")}
-                    >
-                        /
-                    </button>
-                    <button
-                        className="bg-black text-white p-3 rounded-lg"
-                        onClick={() => handleOperator("=")}
-                    >
-                        =
-                    </button>
-                </ul>
+            <div className="flex mx-auto w-52 justify-center font-extrabold text-xl">
+                Calculator
+            </div>
+            <div className="flex-auto mx-auto w-72 bg-slate-300 pt-1 shadow-lg rounded-xl">
+                <div className="flex w-52 bg-white rounded-md mx-auto p-3 my-4 font-bold">
+                    {displayValue}
+                </div>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("1")}
+                >
+                    1
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("2")}
+                >
+                    2
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("3")}
+                >
+                    3
+                </button>
+                <button
+                    className={
+                        "bg-red-950 text-white p-3 rounded-lg m-4 w-10 font-serif hover:bg-red-700"
+                    }
+                    onClick={() => handleOperator("+")}
+                    // style={{
+                    //     backgroundColor: true
+                    //         ? "lightgreen"
+                    //         : "rgb(50 60 90 / var(--tw-bg-opacity))",
+                    // }}
+                >
+                    +
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("4")}
+                >
+                    4
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("5")}
+                >
+                    5
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("6")}
+                >
+                    6
+                </button>
+                <button
+                    className="bg-red-950 text-white p-3 rounded-lg m-4 w-10 font-serif  hover:bg-red-700"
+                    onClick={() => handleOperator("-")}
+                >
+                    -
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("7")}
+                >
+                    7
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("8")}
+                >
+                    8
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("9")}
+                >
+                    9
+                </button>
+                <button
+                    className="bg-red-950 text-white p-3 rounded-lg m-4 w-10 font-serif hover:bg-red-700"
+                    onClick={() => handleOperator("=")}
+                >
+                    =
+                </button>
+                <button
+                    className="bg-black text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleNumber("0")}
+                >
+                    0
+                </button>
+                <button
+                    className="bg-red-950 text-white p-3 rounded-lg m-4 w-10 font-serif hover:bg-red-700"
+                    onClick={() => handleOperator("*")}
+                >
+                    *
+                </button>
+                <button
+                    className="bg-red-950 text-white p-3 rounded-lg m-4 w-10 font-serif hover:bg-red-700"
+                    onClick={() => handleOperator("/")}
+                >
+                    /
+                </button>
+                <button
+                    className="bg-red-500 text-white p-3 rounded-lg m-4 w-10 font-serif"
+                    onClick={() => handleOperator("c")}
+                >
+                    C
+                </button>
             </div>
         </>
     );
